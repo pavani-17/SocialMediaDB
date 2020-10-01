6f555a1790b73a0741675dc565576b34e9affae2
NIRMAL MANOJ C <nimmumanoj@gmail.com>
2020-10-02 00:38:49 +0530

    Main function and view functions for all tables

--- app.py
+++ app.py
@@ -4,3 +4,195 @@ import pymysql.cursors
 from tabulate import tabulate
 
 
+def viewTable(rows):
+
+    a = []
+    try:
+        a.append(list(rows[0].keys()))
+    except:
+        print("\n-----------------\nEMPTY TABLE\n-----------------\n")   
+        return
+    for row in rows:
+        b = []
+        for k in row.keys():
+            b.append(row[k])
+        a.append(b)
+    print(tabulate(a, tablefmt="psql", headers="firstrow"))
+    print()
+    return
+
+
+def viewOptions():
+    print("\nChoose the data that you want to see.\n\n")
+    print("1.  USERS")
+    print("2.  POST")
+    print("3.  STORIES")
+    print("4.  MESSAGES")
+    print("5.  PROFILES")
+    print("6.  EDUCATION OF USERS")
+    #Pages
+    print("7.  PAGES")
+    print("8.  PAGES OF BUSINESS_PLACE")
+    print("9. PRODUCTS OF BRANDS AND DETAILS")
+    print("10. PAGES OF COMPANIES")
+    print("11. BRANCHES OF COMPANIES")
+    print("12. PAGES OF BRAND PRODUCTS")
+    print("13. PAGES OF PUBLIC FIGURES")
+    print("14. NEWS ABOUT PUBLIC FIGURES")
+    print("15. PAGES OF ENTERTAINMENT INDUSTRY ENTITIES")
+    print("16. PAGES OF CAUSE COMMUNITIES")
+    #Groups
+    print("17. GROUPS")
+    #RelatioNships
+    print("18. COMMENTS RELATIONSHIPS")
+    print("19. FOLLOWS RELATIONSHIPS")
+    print("20. GENERAL REACTS TO POSTS")
+    print("21. LIKES TO PAGES")
+    print("22. BELONGS TO GROUP RELATIONSHIP WITH USERS")
+    print("23. ADMINS OF GROUPS")
+    print("24. MODERATORS OF GROUPS")
+    print("25. REACTS TO COMMENTS")
+    print("26. MENTIONS OF USERS IN COMMENTS")
+    print("27. SPECIFIC MESSAGES BETWEEN USERS - META DETAILS")
+    print("28. GENERAL MESSAGES TO GROUPS - META DETAILS")
+    print("29. RESPONDS TO STORIES")
+    print("30. SHARES A POST IN A GROUP") # Basically posts in a group - comment to avoid semantical confusion
+    print("31. USERS TAGGED IN POSTS")
+    print("\n")
+    choice = input("Enter: ")
+
+    if choice == '1':
+        query = "SELECT * FROM USER;"
+    elif choice == '2':
+        query = "SELECT * FROM POST;"
+    elif choice == '3':
+        query = "SELECT * FROM STORIES;"
+    elif choice == '4':
+        query = "SELECT * FROM MESSAGE;"
+    elif choice == '5':
+        query = "SELECT * FROM PROFILE;"
+    elif choice == '6':
+        query = "SELECT * FROM EDUCATION;"
+    # Pages and subclasses
+    elif choice == '7':
+        query = "SELECT * FROM PAGE;"
+    elif choice == '8':
+        query = "SELECT * FROM BUSINESS_PLACE;"
+    elif choice == '9':
+        query = "SELECT * FROM PROD_BP;"
+    elif choice == '10':
+        query = "SELECT * FROM COMPANY;"
+    elif choice == '11':
+        query = "SELECT * FROM BRANCH_COMPANY;"
+    elif choice == '12':
+        query = "SELECT * FROM BRAND_PRODUCT;"
+    elif choice == '13':
+        query = "SELECT * FROM PUBLIC_FIGURE;"
+    elif choice == '14':
+        query = "SELECT * FROM NEWS_PUB_FIG;"
+    elif choice == '15':
+        query = "SELECT * FROM ENTERTAINMENT"
+    elif choice == '16':    
+        query = "SELECT * FROM CAUSE_COMMUNITY"
+    # Pages over
+    elif choice == '17':
+        query = "SELECT * FROM social_media.GROUP"
+    # Relationships
+    elif choice == '18':
+        query = "SELECT * FROM COMMENTS;"
+    elif choice == '19':
+        query = "SELECT * FROM FOLLOWS;"
+    elif choice == '20':
+        query = "SELECT * FROM MAKES_GENERAL_REACT;"
+    elif choice == '21':
+        query = "SELECT * FROM LIKES"
+    elif choice == '22':
+        query = "SELECT * FROM BELONGS_TO"
+    elif choice == '23':
+        query = "SELECT * FROM IS_ADMIN"
+    elif choice == '24':
+        query = "SELECT * FROM IS_MODERATOR"
+    elif choice == '25':
+        query = "SELECT * FROM MAKES_A_REACT;"
+    elif choice == '26':
+        query = "SELECT * FROM MENTIONS;"
+    elif choice == '27':
+        query = "SELECT * FROM SENDS_SPECIFIC;"
+    elif choice == '28':
+        query = "SELECT * FROM SENDS_GENERAL"
+    elif choice == '29':
+        query = "SELECT * FROM RESPONDS"
+    elif choice == '30':
+        query = "SELECT * FROM SHARES"
+    elif choice == '31':
+        query = "SELECT * FROM IS_TAGGED;"
+    else:
+        print("You have entered an invalid option.")
+
+    try:
+        no_of_rows = cur.execute(query)
+    except Exception as e:
+        print(e)
+        print("\n\nError!\n")
+        return
+    
+    rows = cur.fetchall()
+    viewTable(rows)
+    con.commit()
+
+
+
+def refreshDatabase():
+    global cur
+
+    # Deleting incorrectly entered data in insert function
+    # Have to write this function.
+    print("Hello: Refreshing database") #Test printline.
+
+while(1):
+    tmp = sp.call('clear', shell=True)
+    # The two lines below should be uncommented 
+    # username = input("Username: ")
+    # password = input("Password: ")
+
+    username = 'root'
+    password = 'blahblah'
+
+    try:
+        con = pymysql.connect(host='127.0.0.1',
+                              user=username,
+                              password=password,
+                              db='social_media',
+                              cursorclass=pymysql.cursors.DictCursor,
+                              port=5005)
+    except Exception as excep:
+        print(excep)
+        tmp = sp.call('clear', shell=True)
+        print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
+        tmp = input("Enter any key to CONTINUE>")
+        continue
+
+    with con.cursor() as cur:
+        exitflag = 0
+        while(1):
+            tmp = sp.call('clear', shell=True)
+            refreshDatabase()
+            print("CHOOSE AN OPTION\n")
+            print("1.View Options")
+            print("2.Addition Options")
+            print("3.Deletion Options")
+            print("4.Modify Options")
+            print("5.Quit")
+            inp = input("\nENTER: ")
+            if(inp == '1'):
+                viewOptions()
+            elif(inp == '5'):
+                exitflag = 1
+                print("Exiting.")
+                break
+            
+            print("Press enter to continue ... ")
+            input()
+
+    if exitflag == 1:
+        break
